fa41a9e7f31223d7550c339a5dfa0b70bfa42577
esic <esic@DESKTOP-IO0O6IO>
2022-10-13 14:58:07 +0200

    initialisation du projet et application properties

---
+++ Projet_de_fin_de_formation/src/main/java/fr/solutec/ProjetDeFinDeFormationApplication.java
@@ -0,0 +1,13 @@
+package fr.solutec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDeFinDeFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDeFinDeFormationApplication.class, args);
+	}
+
+}
