8a6b779b39c2b2160bb09309fff85af8df9d34ed
mcaroo <caroline7777@hotmail.fr>
2022-10-19 15:32:13 +0200

    amis en cours

--- Projet_de_fin_de_formation/src/main/java/fr/solutec/ProjetDeFinDeFormationApplication.java
+++ Projet_de_fin_de_formation/src/main/java/fr/solutec/ProjetDeFinDeFormationApplication.java
@@ -27,7 +27,7 @@ public class ProjetDeFinDeFormationApplication implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepos;
 	@Autowired
-	private EventRepository eventRepos;	
+	private EventRepository eventRepos;
 	@Autowired
 	private LieuRepository lieuRepos;
 	@Autowired
@@ -39,7 +39,7 @@ public class ProjetDeFinDeFormationApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetDeFinDeFormationApplication.class, args);
-		System.out.println("Main successful");
+		System.out.println("☺♂☺♂☺♂  Run successful create by JoJO  ☺♂☺♂☺♂");
 
 	}
 
@@ -54,56 +54,55 @@ public class ProjetDeFinDeFormationApplication implements CommandLineRunner {
 				10000, 2500, null, null, 0);
 		User u4 = new User(null, "Lajus", "Quentin", "lajqu", "lajqu123", "0600000004", "laquentin@esic.fr", false,
 				10000, 2500, null, null, 0);
-		User u5 = new User(null, "Martinez Alonso", "Caroline", "Caro", "Caroo", "0600000005", "macarolinen@esic.fr", true,
-				20000, 3500, null, null, 0);
+		User u5 = new User(null, "Martinez Alonso", "Caroline", "Caro", "Caroo", "0600000005", "macarolinen@esic.fr",
+				true, 20000, 3500, null, null, 0);
 
 		Stream.of(u1, u2, u3, u4, u5).forEach(u -> {
 			userRepos.save(u);
 		});
 
-		Lieu l1 = new Lieu(null,"France", "Paris", "Rue Francis de Pressensé, Saint-Denis, 93200",
-		"StadedeFance",80000);
-		Lieu l2 = new Lieu(null,"France", "Lille", "17 Place Mendès France, Lille, 59000",
-		"Le Nouveau Siècle",59000);
-		Lieu l3 = new Lieu(null,"Allemagne", "Berlin", "Friedrichstrasse 107, Berlin 10117",
-		"Friedrichstadt-Palast", 10117);
-		Lieu l4 = new Lieu(null,"France", "Nancy", "10 rue Sonnini, Manoncourt-En-Vermois, 54210",
-		"Domaine Des Anges", 54210);
-		
+		Lieu l1 = new Lieu(null, "France", "Paris", "Rue Francis de Pressensé, Saint-Denis, 93200", "StadedeFance",
+				80000);
+		Lieu l2 = new Lieu(null, "France", "Lille", "17 Place Mendès France, Lille, 59000", "Le Nouveau Siècle", 59000);
+		Lieu l3 = new Lieu(null, "Allemagne", "Berlin", "Friedrichstrasse 107, Berlin 10117", "Friedrichstadt-Palast",
+				10117);
+		Lieu l4 = new Lieu(null, "France", "Nancy", "10 rue Sonnini, Manoncourt-En-Vermois, 54210", "Domaine Des Anges",
+				54210);
+
 		Stream.of(l1, l2, l3, l4).forEach(l -> {
 			lieuRepos.save(l);
 		});
-		
-		
 
-		Event e1 = new Event(null, "Indochine", "Concert Indochine The Last Tourney 1", 30000, d.parse("10/12/2022"), "20h", "Concert", 55, null, u3, l1);
-		Event e2 = new Event(null, "Caroline Martinez", "Caro fait son One Woman Show", 6300, d.parse("19/02/2023"), "20h30", "Humour", 15, null, u5, l2);
-		Event e3 = new Event(null, "", "France-Brésil", 50000, d.parse("23/12/2022"), "20h", "Football Masculin", 70, null, u3, l1);
-		Event e4 = new Event(null, "Ahadi Mahaboubi", "Tous à Nancy pour Nöel", 100, d.parse("25/12/2022"), "20h", "Apéritif Dinatoire", 10, null, u3, l4);
+		Event e1 = new Event(null, "Indochine", "Concert Indochine The Last Tourney 1", 30000, d.parse("10/12/2022"),
+				"20h", "Concert", 55, null, u3, l1);
+		Event e2 = new Event(null, "Caroline Martinez", "Caro fait son One Woman Show", 6300, d.parse("19/02/2023"),
+				"20h30", "Humour", 15, null, u5, l2);
+		Event e3 = new Event(null, "", "France-Brésil", 50000, d.parse("23/12/2022"), "20h", "Football Masculin", 70,
+				null, u3, l1);
+		Event e4 = new Event(null, "Ahadi Mahaboubi", "Tous à Nancy pour Nöel", 100, d.parse("25/12/2022"), "20h",
+				"Apéritif Dinatoire", 10, null, u3, l4);
 
-
-		
 		Stream.of(e1, e2, e3, e4).forEach(e -> {
 			eventRepos.save(e);
 		});
-		
+
 		Avantage a1 = new Avantage(null, "Popcorn", 100);
 		Avantage a2 = new Avantage(null, "Masseur Shiatsu", 100);
 		Avantage a3 = new Avantage(null, "Boisson gratuite", 100);
-		
+
 		Stream.of(a1, a2, a3).forEach(a -> {
 			avantageRepos.save(a);
-		});		
-		
+		});
+
 		Achats b1 = new Achats(u1, a1, 2, 200);
 		Achats b2 = new Achats(u2, a2, 1, 100);
 		Achats b3 = new Achats(u1, a3, 5, 500);
-		
+
 		Stream.of(b1, b2, b3).forEach(b -> {
 			achatsRepos.save(b);
-		});	
-		
-		System.out.println("Run successful");
+		});
+
+		System.out.println("****************\n*****************\n☺♂☺♂☺♂  Run sera toujours, haha successful create by JoJO BONUS ****************\n*****************\n☺♂☺♂☺♂****************\n*****************\n");
 	}
 
 }
@@ -111,16 +110,7 @@ public class ProjetDeFinDeFormationApplication implements CommandLineRunner {
 /*
  * structure model Event :
  * 
-	private Long id;
-	private String artiste;
-	private String titre;	
-	private int place;
-	private Date date;
-	private String heure;
-	private String genre;
-	private int prix;
-	private String photo;
-	private User organisateur;
-	private Lieu lieu;
+ * private Long id; private String artiste; private String titre; private int
+ * place; private Date date; private String heure; private String genre; private
+ * int prix; private String photo; private User organisateur; private Lieu lieu;
  */
-
---
+++ Projet_de_fin_de_formation/src/main/java/fr/solutec/entities/Amis.java
@@ -0,0 +1,35 @@
+package fr.solutec.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor 
+@AllArgsConstructor
+@Data
+@Entity
+public class Amis {
+	@Id	@GeneratedValue
+	private Long id; 
+	private Boolean acceptation;
+	
+	@CreationTimestamp 
+	private Date demandDate;
+	
+	@ManyToOne
+	private User demandeur;
+	
+	@ManyToOne
+	private User receveur;
+}
