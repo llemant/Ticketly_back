d784d52d9580112e94ff0ccbcfb3eae637b2d6a9
LiseDR <lisedefo.dev@gmail.com>
2022-10-19 11:30:34 +0200

    Création classes achats avantage

---
+++ Projet_de_fin_de_formation/src/main/java/fr/solutec/entities/Avantage.java
@@ -0,0 +1,22 @@
+package fr.solutec.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+
+public class Avantage {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String avantage;
+	private int prix;
+}
---
+++ Projet_de_fin_de_formation/src/main/java/fr/solutec/repository/AvantageRepository.java
@@ -0,0 +1,9 @@
+package fr.solutec.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import fr.solutec.entities.Avantage;
+
+public interface AvantageRepository extends CrudRepository<Avantage, Long> {
+
+}
